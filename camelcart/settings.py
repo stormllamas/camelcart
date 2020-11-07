35e1f04ec039b7bce38c01885c7f53f596082142
stormllamas <storm_sia_llamas@yahoo.com>
2020-11-07 13:28:26 +0800

    config update

--- camelcart/settings.py
+++ camelcart/settings.py
@@ -12,7 +12,7 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = ')!kys2xasa=-kss$66qj3ph-l@gcn9+^u7te29mjfpaqnijx#^'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
