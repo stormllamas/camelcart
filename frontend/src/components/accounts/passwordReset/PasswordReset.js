3c1d218c986b58c1eabce9d54069cd0762cba747
stormllamas <storm_sia_llamas@yahoo.com>
2020-12-03 12:29:23 +0800

    Fix Password Reset

--- frontend/src/components/accounts/passwordReset/PasswordReset.js
+++ frontend/src/components/accounts/passwordReset/PasswordReset.js
@@ -1,4 +1,4 @@
-import React, { Fragment, useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { HashLink as Link } from 'react-router-hash-link';
 import PropTypes from 'prop-types'
 import { connect } from 'react-redux';
